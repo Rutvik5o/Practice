e1c401205d181b95d130649048b8d9ac4cccad70
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-05-26 18:05:43 +0530

    Class

---
+++ class.py
@@ -0,0 +1,24 @@
+class calc:
+
+    def add(x,y):
+        answer=x+y
+        print(answer)
+
+    def sub(x,y):
+        answer=x-y
+        print(answer)
+
+    def mul(x,y):
+        answer=x*y
+        print(answer)
+
+    def div(x,y):
+        answer=x/y
+        print(answer)
+
+
+
+calc.add(10,20)
+calc.sub(10,20)
+calc.mul(10,20)
+calc.div(10,20)
