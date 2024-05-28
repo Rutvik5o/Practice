52c2e0481c0b6c9dadd3a975d0d9677f23f92f28
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-05-28 21:48:40 +0530

    Import Syntax

---
+++ importsyntax.py
@@ -0,0 +1,17 @@
+#from statistics import mean as s
+#print(s(exlist))
+
+
+#import statistics import mean => for access only specific function
+
+#from statistics import *
+
+from statistics import mean as m, median as me #> can be multiple
+
+
+exlist=[5,35,66,4,336,234,55,44,33]
+
+print(m(exlist))
+
+print(me(exlist))
+
