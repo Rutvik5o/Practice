ad337db7aa4a3e6df1b018d57e8da0613019e04f
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-09-13 21:54:58 +0530

    ASP.NET : 13/09 Practice of Unit-2

---
+++ Four/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        int cnt = 1;
+
+        if (Request.Cookies["MyCk"] != null)
+        {
+            cnt = Convert.ToInt16(Request.Cookies["MyCk"].Value);
+        }
+
+        HttpCookie Obj = new HttpCookie("MyCk");
+
+        Obj.Value = Convert.ToString(Obj);
+
+        Response.Cookies.Add(Obj);
+
+        Label1.Text = "<b> This page is visited " + Convert.ToString(cnt) + "times on this computer</b>";
+    }
+}
