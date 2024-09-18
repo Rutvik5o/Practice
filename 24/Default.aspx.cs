7dcfc4ff11c024a3a8e24a54c932e3f35edc3ef2
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-09-18 18:39:22 +0530

    Once More: Unit 2: (4) : 18/09

---
+++ 24/Default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        int count = 1;
+
+        if (Request.Cookies["MK"] != null)
+        {
+            count = Convert.ToInt16(Request.Cookies["MK"].Value);
+
+            count++;
+        }
+
+        HttpCookie Obj = new HttpCookie("MK");
+
+        Obj.Value = Convert.ToString(count);
+
+        Response.Cookies.Add(Obj);
+
+        Label1.Text = "You have visted total " + Convert.ToString(count) + " imes";
+
+    
+}
+}
